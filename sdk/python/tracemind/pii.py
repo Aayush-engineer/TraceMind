1544e10fabf68b7a0a625d2d5e6fbd2b754cb143
Aayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2026-07-08 21:15:01 +0530

    style(pii): remove redundant class docstring

--- sdk/python/tracemind/pii.py
+++ sdk/python/tracemind/pii.py
@@ -2,7 +2,6 @@ import re
 from typing import Any
 
 class PIIRedactor:
-    """Redacts PII from text before sending to TraceMind."""
 
     _PATTERNS = [
         (re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'), "[EMAIL]"),
